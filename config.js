9c933da17a95d47155d17517340313f46dfb5b9e
formulariosonline <luizfabriciopereira@see.pb.gov.br>
2026-08-27 15:32:26 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// CONFIGURAÇÃO DO SUPABASE
+window.PLAYER_TV_CONFIG = {
+  supabaseUrl: "https://hicopwrkfyfejscodhap.supabase.co",
+  supabaseAnonKey: "sb_publishable_DO2KRNZB8g_fIahlXAhZjA_6Q92Wl5q",
+  bucketName: "media"
+};
